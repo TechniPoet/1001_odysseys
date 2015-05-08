b9dc679be91a8ce6ad7048efcc58d18a468ea655
Jeff Robbins <RobbinsJeffreyM@gmail.com>
2015-05-07 21:12:17 -0400

    Stubbed out button functions

--- Assets/Scripts/Screens/TitleScreen.cs
+++ Assets/Scripts/Screens/TitleScreen.cs
@@ -7,9 +7,40 @@ public class TitleScreen : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	/// <summary>
+	/// Launches Setup for game loop.
+	/// </summary>
+	public void StartGame()
+	{
+
+	}
+
+
+	/// <summary>
+	/// Opens the popup for settings.
+	/// </summary>
+	public void OpenSettings()
+	{
+
+	}
+
+
+	/// <summary>
+	/// Opens game website 
+	/// </summary>
+	public void OpenWebsite()
+	{
+
+	}
+
+
+	/// <summary>
+	/// Opens the rules.
+	/// </summary>
+	public void OpenRules()
+	{
+
 	}
 }
