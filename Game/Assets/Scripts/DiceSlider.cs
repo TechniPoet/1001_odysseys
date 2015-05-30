83c1000514c39d152026f344b8df9ec7aef2f5d5
Jeff Robbins <RobbinsJeffreyM@gmail.com>
2015-05-30 19:12:41 -0400

    Added Roll die screen and re arranged Phase1 to be under one canvas

---
+++ Game/Assets/Scripts/DiceSlider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DiceSlider : MonoBehaviour {
+	Text textBox;
+	Slider thisSlider;
+
+	// Use this for initialization
+	void Start () {
+		textBox = GetComponentInChildren<Text>();
+		thisSlider = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textBox.text = thisSlider.value.ToString();
+	}
+}
